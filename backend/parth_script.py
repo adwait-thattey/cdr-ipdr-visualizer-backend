62eb5bf3a845ced2ba55453a589f30efafa8f87e
Adwait Thattey <coderdude1999@gmail.com>
2020-08-03 01:36:51 +0530

    parth data

--- backend/parth_script.py
+++ backend/parth_script.py
@@ -1,9 +1,9 @@
 from analytics.views import get_community_and_importance
 from data.models import CDR
+import parth_data
+# arr = list(CDR.objects.values_list('from_number', 'to_number', 'duration'))
+# d = [{'from_number':i[0], 'to_number':i[0], 'duration':i[2]} for i in arr]
+print(parth_data.data)
+obj = get_community_and_importance(parth_data.data)
 
-arr = list(CDR.objects.values_list('from_number', 'to_number', 'duration'))
-d = [{'from_number':i[0], 'to_number':i[0], 'duration':i[2]} for i in arr]
-# print(d)
-obj = get_community_and_importance(d)
-
-print(obj)
+# print(obj)
